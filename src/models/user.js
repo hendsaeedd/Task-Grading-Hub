7a18e82e54d618e5fc4e22bdaeae4fde9e4f5875
hendsaeedd <hendsaied135@gmail.com>
2024-02-22 16:34:04 +0200

    add users model

---
+++ src/models/user.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose')
+
+const userSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    unique: true,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  //role (enum: "student", "admin")
+  role: {
+    type: String,
+    enum: ['student', 'admin'],
+    default: 'student',
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+})
+
+const User = mongoose.model('users', userSchema)
+
+module.exports = User
