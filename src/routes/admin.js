7239bd5f028818841ae5aa103a9da62184ced784
hendsaeedd <hendsaied135@gmail.com>
2024-02-23 15:02:46 +0200

    view submitted tasks with user id route

--- src/routes/admin.js
+++ src/routes/admin.js
@@ -1,12 +1,12 @@
 const express = require('express')
 const router = express.Router()
-const { createTask, updateTask, deleteTask } = require('../controllers/admin')
-
-// //register
-// router.post('/register', )
-
-// //login
-// router.post('/login', )
+const {
+  createTask,
+  updateTask,
+  deleteTask,
+  submittedTasks,
+  userIdSubmittedTask,
+} = require('../controllers/admin')
 
 //create a new task
 router.post('/tasks', createTask)
@@ -18,7 +18,10 @@ router.patch('/tasks/:id', updateTask)
 router.delete('/tasks/:id', deleteTask)
 
 //view submitted tasks
-router.get('/tasks/:id/submissions')
+router.get('/tasks/:id/submissions', submittedTasks)
+
+//view submitted tasks with user id
+router.get('/tasks/:id/submissions/:userId', userIdSubmittedTask)
 
 //grade a submission
 router.put('/tasks/:id/submissions/:submissionId/grade')
