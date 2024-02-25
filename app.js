a6fd120f37616d6729c367d6c22181d5b81a2424
hendsaeedd <hendsaied135@gmail.com>
2024-02-25 14:02:30 +0200

    add error handling middleware

--- app.js
+++ app.js
@@ -3,6 +3,11 @@ const dotenv = require('dotenv')
 dotenv.config()
 const app = express()
 const port = process.env.PORT || 5000
+//error handling middleware
+const errorHandler = require('./src/middleware/globalErrorHandling')
+const handleErrors = require('./src/middleware/handleErrors')
+//authorized middleware
+const checkPermission = require('./src/middleware/checkPermission')
 
 const connect = require('./src/config/db')
 //routes
@@ -16,10 +21,14 @@ app.use(express.urlencoded({ extended: true }))
 app.use(express.static('public'))
 app.use(express.json())
 
-//routes
+//mount routes
 app.use('/auth', authRoutes)
-app.use('/admin', adminRoutes)
+app.use('/admin',checkPermission, adminRoutes)
 app.use('/student', studentRoutes)
+
+//error handling middleware
+app.use(errorHandler)
+app.use(handleErrors)
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`)
