76c3d1826424303d4802c9cad88cf26ad4c35945
hendsaeedd <hendsaied135@gmail.com>
2024-02-25 13:53:34 +0200

    add verifyToken middleware

--- src/routes/student.js
+++ src/routes/student.js
@@ -8,16 +8,18 @@ const {
   checkGrades,
 } = require('../controllers/student')
 
+const verifyToken = require('../middleware/verifyToken')
+
 //view tasks
-router.get('/tasks', viewTasks)
+router.get('/tasks', verifyToken, viewTasks)
 
 //get details of a specific task
-router.get('/tasks/:id', specificTask)
+router.get('/tasks/:id', verifyToken, specificTask)
 
 //submit a task file
 router.post('/tasks/:id/submissions', uploadPdf, submitTask)
 
 //check the grades after a task is graded
-router.get('/tasks/:id/submissions/:userId/grade', checkGrades)
+router.get('/tasks/:id/submissions/:userId/grade', verifyToken, checkGrades)
 
 module.exports = router
