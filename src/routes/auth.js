f3039bfa1dc9198f15a8021bab6f6cdd49e592b3
hendsaeedd <hendsaied135@gmail.com>
2024-03-06 13:47:41 +0200

    add swagger documentation for auth endpoints

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -1,13 +1,107 @@
 const express = require('express')
 const router = express.Router()
-const {
-  registerUser,
-  loginUser,
-} = require('../controllers/auth')
+const { registerUser, loginUser } = require('../controllers/auth')
+/**
+ * @swagger
+ * tags:
+ *   name: Authentication
+ *   description: Authenticate a user
+ */
 
+/**
+ * @swagger
+ * /auth/register:
+ *   post:
+ *     summary: Register user
+ *     tags: [Authentication]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               username:
+ *                 type: string
+ *               password:
+ *                 type: string
+ *               role:
+ *                 type: string
+ *     responses:
+ *       '200':
+ *         description: User was registered successfully
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 message:
+ *                   type: string
+ *                 user:
+ *                   type: object
+ *       '400':
+ *         description: Failed to register user
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 error:
+ *                   type: string
+ */
 //register
 router.post('/register', registerUser)
 
+/**
+ * @swagger
+ * /auth/login:
+ *   post:
+ *     summary: Login user
+ *     tags: [Authentication]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               username:
+ *                 type: string
+ *               password:
+ *                 type: string
+ *     responses:
+ *       '200':
+ *         description: User logged in successfully
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 message:
+ *                   type: string
+ *                 user:
+ *                   type: object
+ *                 accessToken:
+ *                   type: string
+ *       '400':
+ *         description: Invalid username or password
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 error:
+ *                   type: string
+ *       '500':
+ *         description: Failed to log in user
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 error:
+ *                   type: string
+ */
 //login
 router.post('/login', loginUser)
 
